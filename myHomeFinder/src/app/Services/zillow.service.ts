f44623746b1476e4d410a0514702ba7da2c54692
Shawn Chandwani <ShawnC7208@gmail.com>
2017-11-24 20:09:21 -0600

    need to figure out mergeMap, this cannot be accessed in nested scope

--- myHomeFinder/src/app/Services/zillow.service.ts
+++ myHomeFinder/src/app/Services/zillow.service.ts
@@ -1,4 +1,4 @@
-import { HttpClient, HttpEvent, HttpInterceptor, HttpRequest } from '@angular/common/http';
+import { HttpClient, HttpEvent, HttpInterceptor, HttpRequest, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from "rxjs/Observable";
 import { map, catchError } from 'rxjs/operators';
@@ -7,12 +7,18 @@ import { map, catchError } from 'rxjs/operators';
 export class ZillowService {
     baseUrl: string = 'http://www.zillow.com/webservice/GetSearchResults.htm?zws-id=';
     zillowId: string = 'X1-ZWz1bd18cvjsaz_8p8sy';
+    ZillowHeader: HttpHeaders = new HttpHeaders();
+    
 
-    constructor(private http: HttpClient) { }
+    constructor(private http: HttpClient) { 
+        this.ZillowHeader.append('Access-Control-Allow-Headers', 'Content-Type');
+        this.ZillowHeader.append('Access-Control-Allow-Methods', 'GET');
+        this.ZillowHeader.append('Access-Control-Allow-Origin', '*');
+    }
 
     getHomeInfoUsingAddress (address: string, cityStateZip: string): Observable<any> {
-        return this.http.get<any>(`${this.baseUrl}<${this.zillowId}>&address=${address}&citystatezip=${cityStateZip}`, 
-        { observe: 'response' });
+        return this.http.get<any>(`${this.baseUrl}${this.zillowId}&address=${address}&citystatezip=${cityStateZip}`,
+        {headers: this.ZillowHeader, observe: 'response', responseType: 'text' as 'json', withCredentials: false});
     }
 
 }
--- myHomeFinder/src/app/gmaps/gmaps.component.ts
+++ myHomeFinder/src/app/gmaps/gmaps.component.ts
@@ -3,10 +3,13 @@ import { BrowserModule } from "@angular/platform-browser";
 
 import { AgmCoreModule, GoogleMapsAPIWrapper, MapsAPILoader, AgmMarker } from '@agm/core';
 import { google } from '@agm/core/services/google-maps-types';
+import { Observable } from "rxjs/Observable";
+import { map, catchError, mergeMap } from 'rxjs/operators';
 
 import { Imarker } from '../Shared/Interfaces'
 import { GMapsService } from '../Services/gmasp.service'
 import { ZillowService } from '../Services/zillow.service';
+import { Parser, parseString } from "xml2js";
 
 @Component({
   selector: 'app-gmaps',
@@ -14,11 +17,12 @@ import { ZillowService } from '../Services/zillow.service';
   styleUrls: ['./gmaps.component.css']
 })
 export class GmapsComponent implements OnInit {
-  lat: number = 32.776664;
-  lng: number = -96.796988;
+  lat: number = 32.882957;
+  lng: number = -97.15937500000001;
   clickedMarker: Imarker = {lat:this.lat,lng:this.lng};
   StreetAddress: any = [];
   CityStateZip: any = [];
+  ZestimateValue: any = [];
 
   constructor(private gMapsService: GMapsService, private zillowService: ZillowService) { 
     //let geocoder = new google.maps.Geocoder();
@@ -37,20 +41,27 @@ export class GmapsComponent implements OnInit {
       console.log(this.lat);
       console.log(this.lng);
 
-      this.gMapsService.getAddressUsingCoordinates(this.lat, this.lng).subscribe(res => { 
+      this.gMapsService.getAddressUsingCoordinates(this.lat, this.lng)
+      .subscribe(res => { 
         console.log(res.body.results[0]);
         this.StreetAddress = res.body.results[0].address_components[0].long_name + ' ' + res.body.results[0].address_components[1].long_name;
         let city = res.body.results[0].formatted_address.split(',')[1];
         let state = res.body.results[0].formatted_address.split(',')[2].split(' ')[1];
         let zip = res.body.results[0].formatted_address.split(',')[2].split(' ')[2];
         this.CityStateZip = city + ', ' + state;
-      });
 
-      //Call zillow API using StreetAddress and city/state/zip
-      this.zillowService.getHomeInfoUsingAddress(this.StreetAddress, this.CityStateZip).subscribe(res => {
-        console.log(res.body);
+        });
+
+        this.zillowService.getHomeInfoUsingAddress(this.StreetAddress, this.CityStateZip).subscribe(res => {
+          console.log((res.body));
+          parseString(res.body, function(err, result) { 
+            console.dir(result["SearchResults:searchresults"].response[0].results[0].result[0]);
+            console.dir(result["SearchResults:searchresults"].response[0].results[0].result[0].zestimate[0].amount[0]._);
+            console.log(result["SearchResults:searchresults"].response[0].results[0].result[0].zestimate[0].amount[0]._);
+            this.ZestimateValue = 0;
+            this.ZestimateValue = String(result["SearchResults:searchresults"].response[0].results[0].result[0].zestimate[0].amount[0]._);
+          })
       });
-      
   }
 
 
